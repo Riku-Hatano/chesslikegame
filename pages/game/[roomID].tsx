785236487581caebcb0726030b72ef5c8db5b771
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-08-12 17:26:19 -0700

    implemented loguser limitation

--- pages/game/[roomID].tsx
+++ pages/game/[roomID].tsx
@@ -7,6 +7,7 @@ const GameRoom = () => {
     const [socketIo, setSocketIo] = useState<any>();
     const [roomID, setRoomID] = useState<string | undefined>(undefined);
     const [username, setUsername] = useState("");
+    const [roomMembers, setRoomMembers] = useState<string[]>([]);
 
     const getRoomMember = () => {
         if(roomID !== undefined && socketIo) {
@@ -14,39 +15,66 @@ const GameRoom = () => {
         }
     }
 
-    useEffect(() => {
+    useEffect(() => { //socketのインスタンスとサーバー側からのデータ送信のリスナーを設定
         const io = socketio("http://localhost:8080");
         // const io = socketio("http://10.2.121.183:8080");
         setSocketIo(io);
 
         io.on("connect", () => {
-            console.log(io.id);
             setUsername(io.id);
+            setRoomMembers((prevRoomMembers) => [...prevRoomMembers, io.id]);
         });
 
-        io.on("rooms", (members: any) => {
+        //リスナー用
+        io.on("receive-members", (members: any) => {
             console.log(Array.from(members));
+            if(Array.from(members).length > 2) {
+                alert("cannot enter room");
+                io.disconnect();
+            } else {
+                setRoomMembers((prevRoomMembers) => [...prevRoomMembers, ...members]);
+            }
+        })
+        io.on("failed-to-login", () => {
+            alert(`${io.id} failed to login`)
         })
 
         return() => {
             io.disconnect();
         }
-    }, []); // This runs only once to establish the socket connection
+    }, []);
 
-    useEffect(() => {
-        if(router.query.roomID !== undefined) {
-            if(socketIo) {
-                socketIo.emit("join-room", router.query.roomID);
-                setRoomID(router.query.roomID as string);
-            }
+    useEffect(() => { //queryの名前に応じてsocketのルームにjoinする。routerとsocketIoの更新が非同期で行われるため、router.queryをリッスンしてuseEffectする必要がある
+        if(router.query.roomID !== undefined && socketIo !== undefined) {
+            socketIo.emit("join-room", router.query.roomID);
+            setRoomID(router.query.roomID as string);
         }
-    }, [socketIo, router.query]); // This updates when socketIo or router.query changes
+        // if(router.query.roomID !== undefined) {
+        //     if(socketIo) {
+        //         socketIo.emit("join-room", router.query.roomID);
+        //         setRoomID(router.query.roomID as string);
+        //     }
+        // }
+    }, [socketIo, router.query]);
 
     return (
         <>
             <p>room: {roomID}</p>
             <p>username: {username}</p>
             <button onClick={getRoomMember}>check room members</button>
+            {
+                roomMembers.length ?
+                <ul>
+                    {
+                        roomMembers.map((member: string, idx: number) => {
+                            return (
+                                <li key={idx}>{ member }</li>
+                            )
+                        })
+                    }
+                </ul>
+                : console.log("no users yet")
+            }
         </>
     )
 }
--- server/index.ts
+++ server/index.ts
@@ -7,15 +7,19 @@ io.on("connection", (socket: any) => {
     console.log(`Connected: ${socket.id}`);
 
     socket.on("join-room", (roomID: string) => {
-        console.log(`${socket.id} joined ${roomID} !`);
-        socket.join(roomID);
+        //この段階でルームの人数が二人以上だった場合、ルームに入れないようにする
+        if(io.sockets.adapter.rooms.get(roomID) === undefined || Array.from(io.sockets.adapter.rooms.get(roomID)).length < 2) {
+            console.log(`${socket.id} joined ${roomID} !`);
+            socket.join(roomID);
+        } else {
+            console.log("failed to login");
+            socket.emit("failed-to-login");
+        }
     })
     socket.on("check-room", (roomID: string) => {
         const rooms = io.sockets.adapter.rooms;
-        const members = rooms.get(roomID);
-        console.log(roomID);
-        console.log(members);
-        socket.emit("rooms", Array.from(members));
+        const members = rooms.get(roomID); //extract items from members Map array.
+        socket.emit("receive-members", Array.from(members));
     })
     socket.on("disconnect", () => {
         console.log(`Disconnected: ${socket.id}`);
